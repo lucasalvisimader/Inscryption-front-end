5f0ed0eca167ef745f92fec10604d955c60630ee
Lucas <105462862+Lucas-Mader@users.noreply.github.com>
2023-09-12 13:44:01 -0300

    doing better navbar

--- src/components/general/nav/Nav.js
+++ src/components/general/nav/Nav.js
@@ -9,7 +9,7 @@ function Navbar() {
     const [user, setUser] = useState({});
 
     async function deleteAccount() {
-
+        UserService.remove(user.id);
     }
 
     useEffect(() => {
@@ -28,54 +28,74 @@ function Navbar() {
         fetchUser();
     }, []);
 
-    if (user && user.authorities) {
-        if (user.authorities[0] === "ADMIN") {
-            return (
-                <Nav id="nav">
-                    <Nav.Item className='title'>Inscryption Trump</Nav.Item>
-                    <Nav.Item>
-                        <Dropdown>
-                            <Dropdown.Toggle className='register_dropdown' variant="success"
-                                             id="dropdown-basic">Register</Dropdown.Toggle>
-                            <Dropdown.Menu>
-                                <Dropdown.Item href="Card-register">Card</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Dropdown>
-                            <Dropdown.Toggle className='show_dropdown' variant="success"
-                                             id="dropdown-basic">Show</Dropdown.Toggle>
-                            <Dropdown.Menu>
-                                <Dropdown.Item href="Show-all-cards">All cards</Dropdown.Item>
-                                <Dropdown.Item href="Show-one-card">One card</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <button className='exit_account' onClick={() => {
-                        }}>Exit Account
-                        </button>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <button className='delete_account' onClick={deleteAccount}>Delete Account</button>
-                    </Nav.Item>
-                </Nav>
-            );
+    return (
+        <Nav id="nav">
+            <Nav.Item className='title'>Inscryption Trump</Nav.Item>
+            <Nav.Item>
+                <Dropdown>
+                    {user.authorities[0] === "PLAYER"} ? (
+                    <Dropdown.Toggle className='register_dropdown' variant="success" id="dropdown-basic">
+                        Register
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        {user.authorities[0] === "ADMIN"} ? (
+                        <Dropdown.Item href="Card-register">
+                            Card
+                        </Dropdown.Item>)
+                        {user && user.authorities} ? (
+                        <Dropdown.Item href="User-register">
+                            User
+                        </Dropdown.Item>)
+                    </Dropdown.Menu>)
+                </Dropdown>
+            </Nav.Item>
+            <Nav.Item>
+                {user.authorities[0] === "ADMIN"} ? (
+                <Dropdown>
+                    <Dropdown.Toggle className='show_dropdown' variant="success" id="dropdown-basic">
+                        Show
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="Show-all-cards">
+                            All cards
+                        </Dropdown.Item>
+                        <Dropdown.Item href="Show-one-card">
+                            One card
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>) : {user.authorities[0] === "PLAYER"} ? (
+                <Nav.Link className='play' href="play">
+                    Play
+                </Nav.Link>)
+            </Nav.Item>
+            {user && user.authorities} ? (
+            <Nav.Item>
+                <button className='exit_account' onClick={() => {}}>
+                    Exit Account
+                </button>
+            </Nav.Item>)
+            {user.authorities[0] === "PLAYER"} ? (
+            <button className='delete_account' onClick={deleteAccount}>
+                Delete Account
+            </button>)
+        </Nav>
+    );
         } else if (user.authorities[0] === "PLAYER") {
             return (
                 <Nav id="nav">
-                    <Nav.Item className='title'>Inscryption Trump</Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link className='play' href="play">Play</Nav.Link>
+                    <Nav.Item className='title'>
+                        Inscryption Trump
                     </Nav.Item>
+
                     <Nav.Item>
-                        <button className='exit_account' onClick={() => {
-                        }}>Exit Account
+                        <button className='exit_account' onClick={() => {}}>
+                            Exit Account
                         </button>
                     </Nav.Item>
                     <Nav.Item>
-                        <button className='delete_account' onClick={deleteAccount}>Delete Account</button>
+                        <button className='delete_account' onClick={deleteAccount}>
+                            Delete Account
+                        </button>
                     </Nav.Item>
                 </Nav>
             );
@@ -83,16 +103,23 @@ function Navbar() {
     } else {
         return (
             <Nav id="nav">
-                <Nav.Item className='title'>Inscryption Trump</Nav.Item>
+                <Nav.Item className='title'>
+                    Inscryption Trump
+                </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link className='login' href="login">Login</Nav.Link>
+                    <Nav.Link className='login' href="login">
+                        Login
+                    </Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                     <Dropdown>
-                        <Dropdown.Toggle className='register_dropdown' variant="success"
-                                         id="dropdown-basic">Register</Dropdown.Toggle>
+                        <Dropdown.Toggle className='register_dropdown' variant="success" id="dropdown-basic">
+                            register
+                        </Dropdown.Toggle>
                         <Dropdown.Menu>
-                            <Dropdown.Item href="User-register">User</Dropdown.Item>
+                            <Dropdown.Item href="User-register">
+                                User
+                            </Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
                 </Nav.Item>
