ffb2ff332eb4db333819da7e0895f2aa36adbee5
Lucas <105462862+Lucas-Mader@users.noreply.github.com>
2023-10-15 14:41:42 -0300

    renaming archives, changes in cursor and css from tv filters

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,12 +1,24 @@
 // styles
 import "./Menu.css"
 
+// react
+import { useState } from "react";
+
+// images
+import continueText from '../../assets/images/menu/texts/continue.png'
+import optionsText from '../../assets/images/menu/texts/options.png'
+
 const Menu = () => {
+    const [textSelected, setTextSelected] = useState('');
+
+    const handleHoverCard = (text) => {
+        setTextSelected(text);
+    }
 
     return (<>
         <div className="menu_container">
             <div className="menu_header">
-
+                <img className="menu_card_text_selected" src={textSelected} />
             </div>
             <div className="menu_body_container">
                 <div className="menu_body">
@@ -15,8 +27,12 @@ const Menu = () => {
             </div>
             <div className="menu_footer">
                 <div className="menu_cards" id="menu_new_game_card" />
-                <div className="menu_cards" id="menu_continue_card" />
-                <div className="menu_cards" id="menu_options_card" />
+                <div className="menu_cards" id="menu_continue_card"
+                    onMouseEnter={() => handleHoverCard(continueText)}
+                    onMouseLeave={() => handleHoverCard('')} />
+                <div className="menu_cards" id="menu_options_card"
+                    onMouseEnter={() => handleHoverCard(optionsText)}
+                    onMouseLeave={() => handleHoverCard('')} />
             </div>
         </div>
     </>);
