0f496bb09d91d2ec6eeca1db908b3749393ff3c1
Lucas <105462862+Lucas-Mader@users.noreply.github.com>
2023-10-19 20:54:39 -0300

    selected language update

--- src/components/modal_options_card/ModalOptionsCard.js
+++ src/components/modal_options_card/ModalOptionsCard.js
@@ -12,15 +12,25 @@ import { Modal } from "react-bootstrap";
 
 const ModalOptionsCard = ({ show }) => {
     const languageOptions = en.languages;
-    const [selectedOption, setSelectedOption] = useState(languageOptions[0]);
+    const [selectedOption, setSelectedOption] = useState(0);
+    const [languageChosen, setLanguageChosen] = useState(languageOptions[selectedOption]);
 
-    const handleChangeLanguage = () => {
-        
+    const handleChangeLanguage = (isNext) => {
+        let newSelectedOption;
+
+        if (isNext) {
+            newSelectedOption = selectedOption < 2 ? selectedOption + 1 : 0;
+        } else {
+            newSelectedOption = selectedOption > 0 ? selectedOption - 1 : 2;
+        }
+
+        setSelectedOption(newSelectedOption);
+        setLanguageChosen(languageOptions[newSelectedOption]);
     }
 
     return (<>
         <Modal className="modal_options_card_container"
-            id="main_modal"
+            id="modal_options_card_container"
             size="sm"
             show={show}
             aria-labelledby="contained-modal-title-vcenter"
@@ -29,9 +39,17 @@ const ModalOptionsCard = ({ show }) => {
             <Modal.Body>
                 <label>Aあ</label>
                 <div className="modal_options_card_language_container">
-                    <button>{'<'}</button>
-                    <div>{selectedOption}</div>
-                    <button>{'>'}</button>
+                    <button className="modal_options_card_button"
+                        onClick={() => handleChangeLanguage(false)}>
+                        {'<'}
+                    </button>
+                    <span className="modal_options_card_language_chosen">
+                        {languageChosen}
+                    </span>
+                    <button className="modal_options_card_button"
+                        onClick={() => handleChangeLanguage(true)}>
+                        {'>'}
+                    </button>
                 </div>
             </Modal.Body>
         </Modal>
